f6200b99ab7fcc2e8b4d5e192772176f7561224c
ROHIT MODI <rohitkumar970ss@gmail.com>
2025-04-09 17:19:17 +0530

    fixed issue in authOption export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -83,6 +83,8 @@ export const authOptions: NextAuthOptions = {
   }
 };
 
+// Create the NextAuth handler
 const handler = NextAuth(authOptions);
 
+// Export the handler as GET and POST handlers
 export { handler as GET, handler as POST };
