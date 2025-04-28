5a9090df2545ed51c8639ae81581bf71ed84b399
ROHIT MODI <rohitkumar970ss@gmail.com>
2025-04-28 09:37:56 +0530

    updated the media-utils

--- lib/media-utils.ts
+++ lib/media-utils.ts
@@ -1,31 +1,7 @@
 // lib/media-utils.ts
-import JournalMedia from '@/models/JournalMedia';
-import Journal from '@/models/JournalModel';
-/**
- * Generates a unique media ID based on journal ID with an incremented number
- * Format: journalId_1, journalId_2, etc.
- */
-export const generateMediaId = async (journalId: string): Promise<string> => {
-  // Find the highest mediaId for this journalId
-  const highestMedia = await JournalMedia.findOne(
-    { journalId, mediaId: { $regex: `^${journalId}_` } },
-    { mediaId: 1 },
-    { sort: { mediaId: -1 } }
-  );
 
-  let nextNumber = 1;
-  
-  if (highestMedia) {
-    // Extract the number part after journalId + "_"
-    const match = highestMedia.mediaId.match(new RegExp(`^${journalId}_(\\d+)$`));
-    if (match && match[1]) {
-      nextNumber = parseInt(match[1], 10) + 1;
-    }
-  }
-  
-  // Format: journalId_1, journalId_2, etc.
-  return `${journalId}_${nextNumber}`;
-};
+import Journal from '@/models/JournalModel';
+
 
 /**
  * Check if the media upload is within limits
@@ -53,20 +29,9 @@ export const checkMediaLimits = async (
     throw new Error(`File size exceeds the limit for ${mediaType}`);
   }
   
-  // Count existing media of this type
-  const existingMediaCount = await JournalMedia.countDocuments({
-    journalId,
-    mediaType: mediaType,
-  });
-  
-  if (existingMediaCount >= MEDIA_LIMITS[typeSafe].count) {
-    throw new Error(
-      `You have reached the limit of ${MEDIA_LIMITS[typeSafe].count} ${mediaType} files for this journal`
-    );
-  }
-  
   return true;
 };
+
 // Check if the journal exists and belongs to the current user
 export const validateJournalAccess = async (journalId: string, userId: number): Promise<boolean> => {
     const journal = await Journal.findOne({ journalId });
